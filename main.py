8be9911d8f3c120bcb7b1c14205ce06a743edcee
Anirudh Ramachandran <annu855201@gmail.com>
2021-09-02 13:19:02 -0700

    Added greet function to Anirudh's page.

--- main.py
+++ main.py
@@ -41,7 +41,13 @@ def michael():
 
 @app.route('/anirudh/')
 def anirudh():
-    return render_template('anirudh.html')
+    # submit button has been pushed
+    if request.form:
+        name = request.form.get("name")
+        if len(name) != 0:  # input field has content
+            return render_template("anirudh.html", name=name)
+    # starting and empty input default
+    return render_template("anirudh.html", name="World")
 
 @app.route('/ethan/')
 def ethan():
