ffc03ea8edf1b022a9a1026ffbf80e96da73f6ba
NastyEthan <89223599+NastyEthan@users.noreply.github.com>
2021-09-24 23:04:33 -0700

    Image resize hack started - still need fix

--- algorithms/image.py
+++ algorithms/image.py
@@ -5,6 +5,7 @@ import base64
 from io import BytesIO
 
 
+
 # image (PNG, JPG) to base64 conversion (string), learn about base64 on wikipedia https://en.wikipedia.org/wiki/Base64
 def image_base64(img, img_type):
     with BytesIO() as buffer:
@@ -196,12 +197,13 @@ def anirudh_image_data(path="static/assets/anirudhimages/", img_list=None):  # c
     return img_list  # list is returned with all the attributes for each image dictionary
 
 def ethan_image_data(path="static/assets/ethanimages/", img_list=None):  # change to ethanimages plz
+    global img_reference
     if img_list is None:  # color_dict is defined with defaults
         img_list = [
             # add your own
-            {'source': "Van Theo", 'label': "GatsVo", 'file': "gatsVo.png"},
-            {'source': "Anirudh", 'label': "POV:You're Under 16", 'file': "anirudhsus.png"},
-            {'source': "Hamilton", 'label': "Evo", 'file': "vomilton.png"},
+            # {'source': "Van Theo", 'label': "GatsVo", 'file': "gatsVo.png"},
+            # {'source': "Anirudh", 'label': "POV:You're Under 16", 'file': "anirudhsus.png"},
+            # {'source': "Hamilton", 'label': "Evo", 'file': "vomilton.png"},
         ]
     # gather analysis data and meta data for each image, adding attributes to each row in table
     for img_dict in img_list:
@@ -209,35 +211,48 @@ def ethan_image_data(path="static/assets/ethanimages/", img_list=None):  # chang
         file = path + img_dict['file']  # file with path for local access (backend)
         # Python Image Library operations
         img_reference = Image.open(file)  # PIL
-        img_data = img_reference.getdata()  # Reference https://www.geeksforgeeks.org/python-pil-image-getdata/
-        img_dict['format'] = img_reference.format
-        img_dict['mode'] = img_reference.mode
-        img_dict['size'] = img_reference.size
-        # Conversion of original Image to Base64, a string format that serves HTML nicely
-        img_dict['base64'] = image_formatter(img_reference, img_dict['format'])
-        # Numpy is used to allow easy access to data of image, python list
-        img_dict['data'] = numpy.array(img_data)
-        img_dict['hex_array'] = []
-        img_dict['binary_array'] = []
-        # 'data' is a list of RGB data, the list is traversed and hex and binary lists are calculated and formatted
-        for pixel in img_dict['data']:
-            # hexadecimal conversions
-            hex_value = hex(pixel[0])[-2:] + hex(pixel[1])[-2:] + hex(pixel[2])[-2:]
-            hex_value = hex_value.replace("x", "0")
-            img_dict['hex_array'].append("#" + hex_value)
-            # binary conversions
-            bin_value = bin(pixel[0])[2:].zfill(8) + " " + bin(pixel[1])[2:].zfill(8) + " " + bin(pixel[2])[2:].zfill(8)
-            img_dict['binary_array'].append(bin_value)
-        # create gray scale of image, ref: https://www.geeksforgeeks.org/convert-a-numpy-array-to-an-image/
-        img_dict['gray_data'] = []
-        for pixel in img_dict['data']:
-            average = (pixel[0] + pixel[1] + pixel[2]) // 3
-            if len(pixel) > 3:
-                img_dict['gray_data'].append((average, average, average, pixel[3]))
-            else:
-                img_dict['gray_data'].append((average, average, average))
-        img_reference.putdata(img_dict['gray_data'])
-        img_dict['base64_GRAY'] = image_formatter(img_reference, img_dict['format'])
+        img = Image.open(file)  # opens file to work
+        clear = img.copy()  # creates a copy of the file used
+        clear = clear.resize(420,69)
+        clear.save(path + 'new' + img_dict['file'])  # saves clean copy as "new<file>.jpg"
+        # appending to img_list so the images can load on the html
+    img_list.append({'source': "Van Theo", 'label': "GatsVo", 'file': "newgatsVo.png"},)
+    img_list.append({'source': "Anirudh", 'label': "POV:You're Under 16", 'file': "newanirudhsus.png"},)
+    img_list.append({'source': "Hamilton", 'label': "Evo", 'file': "newvomilton.png"})
+
+    for img_dict in img_list:
+        img_dict['path'] = '/' + path  # path for HTML access (frontend)
+        file = path + img_dict['file']  # file with path for local access (backend)
+        
+    img_data = img_reference.getdata()  # Reference https://www.geeksforgeeks.org/python-pil-image-getdata/
+    img_dict['format'] = img_reference.format
+    img_dict['mode'] = img_reference.mode
+    img_dict['size'] = img_reference.size
+    # Conversion of original Image to Base64, a string format that serves HTML nicely
+    img_dict['base64'] = image_formatter(img_reference, img_dict['format'])
+    # Numpy is used to allow easy access to data of image, python list
+    img_dict['data'] = numpy.array(img_data)
+    img_dict['hex_array'] = []
+    img_dict['binary_array'] = []
+    # 'data' is a list of RGB data, the list is traversed and hex and binary lists are calculated and formatted
+    for pixel in img_dict['data']:
+        # hexadecimal conversions
+        hex_value = hex(pixel[0])[-2:] + hex(pixel[1])[-2:] + hex(pixel[2])[-2:]
+        hex_value = hex_value.replace("x", "0")
+        img_dict['hex_array'].append("#" + hex_value)
+        # binary conversions
+        bin_value = bin(pixel[0])[2:].zfill(8) + " " + bin(pixel[1])[2:].zfill(8) + " " + bin(pixel[2])[2:].zfill(8)
+        img_dict['binary_array'].append(bin_value)
+    # create gray scale of image, ref: https://www.geeksforgeeks.org/convert-a-numpy-array-to-an-image/
+    img_dict['gray_data'] = []
+    for pixel in img_dict['data']:
+        average = (pixel[0] + pixel[1] + pixel[2]) // 3
+        if len(pixel) > 3:
+            img_dict['gray_data'].append((average, average, average, pixel[3]))
+        else:
+            img_dict['gray_data'].append((average, average, average))
+    img_reference.putdata(img_dict['gray_data'])
+    img_dict['base64_GRAY'] = image_formatter(img_reference, img_dict['format'])
     return img_list  # list is returned with all the attributes for each image dictionary
 
 def james_image_data(path="static/assets/jamesimages/", img_list=None):  # change to jamesimages
