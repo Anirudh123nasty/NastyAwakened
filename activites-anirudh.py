167b091df0edd488ec67feb85cd4d801053237fc
Anirudh Ramachandran <annu855201@gmail.com>
2021-10-14 01:19:20 -0700

    forgot json part

--- activites-anirudh.py
+++ activites-anirudh.py
@@ -5,6 +5,7 @@
 # print(student_list[:])
 # if 'rob' in student_list:
 #     print("Yes, rob is in the student list")
+import json
 
 
 p1 = { "name":"John", "age":61, "city":"Eugene"}
@@ -23,3 +24,27 @@ dict_people = {'people': list_of_people}
 # print(type(dict_people))
 print(dict_people)
 # write some code to Print People from Dictionary
+
+# turn dictionary to JSON
+print("** Dumps - Python to JSON String**")
+json_people = json.dumps(list_of_people)
+print("JSON People #1")
+print(type(json_people))
+print(json_people)
+# write some code to pretty print the JSON dict
+
+
+# write some code to unwind JSON using json.loads and print the people
+print("** Loads - JSON to Python Dict**")
+json_dict = json.loads(json_people)
+print(json_dict)
+# to list
+names = [person['name'] for person in json_dict]
+print("Names of people to list: " + str(names))
+print("Names of people: ")
+# pretty print Names of People
+
+
+
+
+
