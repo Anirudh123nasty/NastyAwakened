8b86678b783f9ca98f96e349b27a29f86fb31848
Anirudh Ramachandran <annu855201@gmail.com>
2021-10-13 22:37:19 -0700

    messed with activities

---
+++ activites-anirudh.py
@@ -0,0 +1,25 @@
+# student_list = ['pam','rob','joe','greg','bob','amy','matt']
+# print(student_list[2:5])
+# print(student_list[:-5])
+# print(student_list[5:])
+# print(student_list[:])
+# if 'rob' in student_list:
+#     print("Yes, rob is in the student list")
+
+
+p1 = { "name":"John", "age":61, "city":"Eugene"}
+p2 = { "name":"Risa", "age":16, "city":"New York"}
+p3 = { "name":"Ryan", "age":16, "city":"Los Angeles"}
+p4 = { "name":"Shekar", "age":16, "city":"San Francisco"}
+# a list of dictionaries
+list_of_people = [p1, p2, p3, p4]
+# write some code to Print List of people one by one
+# print(list_of_people[:])
+
+
+# turn list to dictionary of people
+dict_people = {'people': list_of_people}
+# print("List to Dictionary of people")
+# print(type(dict_people))
+print(dict_people)
+# write some code to Print People from Dictionary
