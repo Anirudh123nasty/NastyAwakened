c886091f7c9a4efd349935ae740ae1f7dd763352
AD1616 <AD1616@users.noreply.github.com>
2021-11-17 15:05:22 -0800

    fix

--- main.py
+++ main.py
@@ -9,9 +9,7 @@ app = Flask(__name__)
 def index():
     return render_template("nasty.html")
 
-@app.route('/README/')
-def README():
-    return render_template("README.html")
+
 
 @app.route('/michael/', methods=['GET', 'POST'])
 def michael():
@@ -41,17 +39,10 @@ def ethan():
             # starting and empty input default
     return render_template("ethan.html", name="World")
 
+
 @app.route('/sahil/', methods=['GET', 'POST'])
-def james():
-    if request.form:
-        name = request.form.get("name")
-        if len(name) != 0:  # input field has content
-            return render_template("james.html", name=name)
-    # starting and empty input default
-    return render_template("sahil.html", name="World")
-@app.route('/sahilabout/', methods=['GET', 'POST'])
 def sahil():
-    return render_template("sahilabout.html", pic1="/static/assets/sahilimages/sahilmask.JPG", pic2="/static/assets/sahilimages/sahilnomask.JPG")
+    return render_template("sahil.html", pic1="/static/assets/sahilimages/sahilmask.JPG", pic2="/static/assets/sahilimages/sahilnomask.JPG")
 
 @app.route('/nasty/')
 def nasty():
